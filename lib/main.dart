8cb3c447728136ee7dc5731c7e6ee678b7444934
IffatFarzana <iffatfarzana1182@gmail.com>
2024-09-20 23:07:49 +0600

    'flutter_application_1'

---
+++ lib/main.dart
@@ -0,0 +1,172 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(CalculatorApp());
+}
+
+class CalculatorApp extends StatelessWidget {
+  const CalculatorApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Calculator',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: CalculatorHomePage(),
+    );
+  }
+}
+
+class CalculatorHomePage extends StatefulWidget {
+  @override
+  _CalculatorHomePageState createState() => _CalculatorHomePageState();
+}
+
+class _CalculatorHomePageState extends State<CalculatorHomePage> {
+  String output = "0";
+
+  String _output = "0";
+  double num1 = 0.0;
+  double num2 = 0.0;
+  String operand = "";
+
+  buttonPressed(String buttonText) {
+    if (buttonText == "DEL") {
+      _output = "0";
+      num1 = 0.0;
+      num2 = 0.0;
+      operand = "";
+    } else if (buttonText == "+" ||
+        buttonText == "-" ||
+        buttonText == "X" ||
+        buttonText == "/") {
+      num1 = double.parse(output);
+
+      operand = buttonText;
+
+      _output = "0";
+    } else if (buttonText == ".") {
+      if (_output.contains(".")) {
+        return;
+      } else {
+        _output = _output + buttonText;
+      }
+    } else if (buttonText == "=") {
+      num2 = double.parse(output);
+
+      if (operand == "+") {
+        _output = (num1 + num2).toString();
+      }
+      if (operand == "-") {
+        _output = (num1 - num2).toString();
+      }
+      if (operand == "X") {
+        _output = (num1 * num2).toString();
+      }
+      if (operand == "/") {
+        _output = (num1 / num2).toString();
+      }
+
+      num1 = 0.0;
+      num2 = 0.0;
+      operand = "";
+    } else {
+      _output = _output + buttonText;
+    }
+
+    setState(() {
+      output = double.parse(_output).toStringAsFixed(2);
+    });
+  }
+
+  Widget buildButton(String buttonText, Color color) {
+    return Expanded(
+      child: Container(
+        margin: const EdgeInsets.all(8),
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            padding: const EdgeInsets.all(25.0), backgroundColor: color,
+          ),
+          child: Text(
+            buttonText,
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          onPressed: () => buttonPressed(buttonText),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Calculator'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            alignment: Alignment.centerRight,
+            padding: EdgeInsets.symmetric(vertical: 24.0, horizontal: 12.0),
+            child: Text(
+              output,
+              style: TextStyle(
+                fontSize: 48.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Expanded(
+            child: Divider(),
+          ),
+          Column(
+            children: [
+              Row(
+                children: <Widget>[
+                  buildButton("1", Colors.orange),
+                  buildButton("2", Colors.orange),
+                  buildButton("3", Colors.orange),
+                  buildButton("/", Colors.green),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  buildButton("4", Colors.orange),
+                  buildButton("5", Colors.orange),
+                  buildButton("6", Colors.orange),
+                  buildButton("-", Colors.green),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  buildButton("7", Colors.orange),
+                  buildButton("8", Colors.orange),
+                  buildButton("9", Colors.orange),
+                  buildButton("X", Colors.green),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  buildButton("0", Colors.orange),
+                  buildButton("%", Colors.green),
+                  buildButton("DEL", Colors.blue),
+                  buildButton("+", Colors.green),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  buildButton("=", Colors.blue),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
